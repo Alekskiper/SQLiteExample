e802622b45c16679ed62fb4ccca7f087762b8609
Alex <nik.alexandr@ufaga.ru>
2019-04-12 20:24:33 +0500

    connect to SQLite

--- app/src/main/java/com/example/sqliteexample/MainActivity.java
+++ app/src/main/java/com/example/sqliteexample/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.sqliteexample;
-
+import java.io.*;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,16 +12,47 @@ public class MainActivity extends AppCompatActivity {
 
     Button button;
     TextView textView;
+
+   private DatabaseHelper mDBHelper;
+   private SQLiteDatabase mDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDBHelper = new DatabaseHelper(this);
+
+        try {
+            mDBHelper.updateDataBase();
+        }
+        catch (IOException mIOException)
+        {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        }
+        catch (SQLException mSQLException)
+        {
+            throw mSQLException;
+        }
+
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                 String product = "";
+                 Cursor cursor = mDb.rawQuery("SELECT * FROM clients", null);
+                 cursor.moveToFirst();
+                 while (!cursor.isAfterLast()) {
+                     product += cursor.getString(1) + " | ";
+                     cursor.moveToNext();
+                 }
+                 cursor.close();
+                 textView.setText(product);
             }
         });
     }
