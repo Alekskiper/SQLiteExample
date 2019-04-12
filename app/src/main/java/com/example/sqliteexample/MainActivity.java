6d441cba9b0e33c942d0beb5ccfe20223a35eae8
Alex <nik.alexandr@ufaga.ru>
2019-04-12 21:16:35 +0500

    work!

--- app/src/main/java/com/example/sqliteexample/MainActivity.java
+++ app/src/main/java/com/example/sqliteexample/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.sqliteexample;
 import java.io.*;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
+    //Объявим переменные компонентов
     Button button;
     TextView textView;
 
-   private DatabaseHelper mDBHelper;
-   private SQLiteDatabase mDb;
+    //Переменная для работы с БД
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,36 +34,72 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             mDBHelper.updateDataBase();
-        }
-        catch (IOException mIOException)
-        {
+        } catch (IOException mIOException) {
             throw new Error("UnableToUpdateDatabase");
         }
 
         try {
             mDb = mDBHelper.getWritableDatabase();
-        }
-        catch (SQLException mSQLException)
-        {
+        } catch (SQLException mSQLException) {
             throw mSQLException;
         }
 
+        //Найдем компоненты в XML разметке
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
+
+        //Пропишем обработчик клика кнопки
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 String product = "";
-                 Cursor cursor = mDb.rawQuery("SELECT * FROM clients", null);
-                 cursor.moveToFirst();
-                 while (!cursor.isAfterLast()) {
-                     product += cursor.getString(1) + " | ";
-                     cursor.moveToNext();
-                 }
-                 cursor.close();
-                 textView.setText(product);
+                String product = "";
+
+                Cursor cursor = mDb.rawQuery("SELECT * FROM clients", null);
+                cursor.moveToFirst();
+                while (!cursor.isAfterLast()) {
+                    product += cursor.getString(1) + " | ";
+                    cursor.moveToNext();
+                }
+                cursor.close();
+
+                textView.setText(product);
             }
         });
-    }
 
+        //Список клиентов
+        ArrayList<HashMap<String, Object>> clients = new ArrayList<HashMap<String, Object>>();
+
+        //Список параметров конкретного клиента
+        HashMap<String, Object> client;
+
+        //Отправляем запрос в БД
+        Cursor cursor = mDb.rawQuery("SELECT * FROM clients", null);
+        cursor.moveToFirst();
+
+//Пробегаем по всем клиентам
+        while (!cursor.isAfterLast()) {
+            client = new HashMap<String, Object>();
+
+            //Заполняем клиента
+            client.put("name", cursor.getString(1));
+            client.put("age", cursor.getString(2));
+
+            //Закидываем клиента в список клиентов
+            clients.add(client);
+
+            //Переходим к следующему клиенту
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+//Какие параметры клиента мы будем отображать в соответствующих
+//элементах из разметки adapter_item.xml
+        String[] from = {"name", "age"};
+        int[] to = {R.id.textView, R.id.textView2};
+
+//Создаем адаптер
+        SimpleAdapter adapter = new SimpleAdapter(this, clients, R.layout.adapter_item, from, to);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+    }
 }
